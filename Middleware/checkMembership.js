86acecaa11f21281d3700ac7e7c3c34e81229f8b
Hawraa Mohammed <nicehooor@gmail.com>
2026-09-13 10:28:11 +0300

    finalize membership checker and add it to routes

--- Middleware/checkMembership.js
+++ Middleware/checkMembership.js
@@ -1,8 +1,12 @@
-const Task = require("../Models/project")
+const Project = require("../Models/project");
 
 const checkMembership = async (req, res, next) => {
-    const project = await Task.findById(req.params.projectId);
-    if (!project.members.user._id.contains(req.user._id)) {
+    const project = await Project.findById(req.params.projectId);
+
+    const isMember = project.members.some((member) =>
+        member.user.toString() === req.user._id)
+
+    if (!isMember) {
         res.status(403).json('You are not authorized to be in this project')
     }
     next();
--- Routes/taskRouter.js
+++ Routes/taskRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const taskCtrl = require('../Controllers/taskCtrl');
 const { checkTaskOwner } = require('../Middleware/checkTaskOwner');
+const { checkMembership } = require('../Middleware/checkMembership');
 const router = express.Router({ mergeParams: true });
 
-router.post('/', taskCtrl.createTask);
-router.get('/', taskCtrl.index);
-router.get('/:taskId', taskCtrl.show);
+router.post('/', checkMembership, taskCtrl.createTask);
+router.get('/', checkMembership, taskCtrl.index);
+router.get('/:taskId', checkMembership, taskCtrl.show);
 router.put('/:taskId', checkTaskOwner, taskCtrl.updateTask);
 
 
