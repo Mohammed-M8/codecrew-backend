c36a5be38262e383c2fadeab9b5c3ff83e5e073a
Hawraa Mohammed <nicehooor@gmail.com>
2026-09-13 09:44:46 +0300

    create get a single task function

--- Controllers/taskCtrl.js
+++ Controllers/taskCtrl.js
@@ -11,4 +11,14 @@ const createTask = async (req, res) => {
         res.status(500).json(err.message);
     }
 }
-module.exports = { createTask };
+const getTask = async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.taskId);
+
+        res.status(200).json(task);
+    }
+    catch (err) {
+        res.status(500).json(err.message);
+    }
+}
+module.exports = { createTask, getTask };
--- Routes/taskRouter.js
+++ Routes/taskRouter.js
@@ -3,5 +3,7 @@ const taskCtrl = require('../Controllers/taskCtrl');
 const router = express.Router({ mergeParams: true });
 
 router.post('/', taskCtrl.createTask);
+router.get('/:taskId', taskCtrl.getTask);
+
 
 module.exports = router;
