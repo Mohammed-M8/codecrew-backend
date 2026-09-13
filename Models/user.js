e6564dd9aa03bc1e6ed3789f15e8feeca1666a12
Mohammed-M8 <202200692@student.polytechnic.bh>
2026-09-13 13:58:39 +0300

    removed test code from user.js

--- Models/user.js
+++ Models/user.js
@@ -14,7 +14,6 @@ const userSchema = new mongoose.Schema({
 userSchema.set('toJSON', {
     transform: (document, userObj) => {
         delete userObj.password;
-        userObj.hello = 'world';
     },
 });
 
