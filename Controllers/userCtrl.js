c08c0daab8cea566b176604ac6cc850bbbe06f4c
Hawraa Mohammed <nicehooor@gmail.com>
2026-09-14 14:40:27 +0300

    add populate project to the activity query

--- Controllers/userCtrl.js
+++ Controllers/userCtrl.js
@@ -9,7 +9,7 @@ const activity = async (req, res) => {
             project: { $in: projectIds }
             , assignedTo: req.user._id,
             status: { $in: ['todo', 'in-progress'] }
-        })
+        }).populate('project');
         res.status(200).json(tasks)
 
     }
